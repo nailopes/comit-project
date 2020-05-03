8aa3875dcdf8faef27fe9721cf1c6571f25b56f2
Naiara <naiara@Naiaras-MacBook-Pro.local>
2020-05-02 20:38:51 -0600

    Updates

---
+++ precache-manifest.aafbd693b9f19af8abb7d7300eca167c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17ab42c46318f32d8846",
+    "url": "/comit-project/static/css/main.2fbb7d5e.chunk.css"
+  },
+  {
+    "revision": "17ab42c46318f32d8846",
+    "url": "/comit-project/static/js/main.17ab42c4.chunk.js"
+  },
+  {
+    "revision": "cd6cfeb1a2224e9ebc54",
+    "url": "/comit-project/static/css/1.5d652f58.chunk.css"
+  },
+  {
+    "revision": "cd6cfeb1a2224e9ebc54",
+    "url": "/comit-project/static/js/1.cd6cfeb1.chunk.js"
+  },
+  {
+    "revision": "162047d720bda42a6f81",
+    "url": "/comit-project/static/js/runtime~main.162047d7.js"
+  },
+  {
+    "revision": "4dc26c4f765cf6738f00be031c29694b",
+    "url": "/comit-project/index.html"
+  }
+];
