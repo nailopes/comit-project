25c787f423dc8cb05e776c10e38cc71ffbfd8770
Naiara <naiara@Naiaras-MacBook-Pro.local>
2020-05-03 23:53:29 -0600

    add shelter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,12 +8,17 @@ const Main = ({ album, totalNumberOfAnimals, calgaryNumberOfAnimals, isLoaded })
         <main role="main">
             <Jumbotron className="text-center">
         
-                <Container hidden={!isLoaded}>
-                    <h1 className="jumbotron-heading">There are at least {totalNumberOfAnimals}  pets avaible for adoption in North America. {calgaryNumberOfAnimals} of them are in Calgary. </h1>
+                <Container >
+                    <h1 className="jumbotron-heading">There are at least</h1>
+                    <h1 className="jumbotron-heading2" hidden={!isLoaded}>{totalNumberOfAnimals}</h1>
+                    <div class="loading" hidden={isLoaded}><h1>Loading&#8230;</h1></div>
+                    <h1 className="jumbotron-heading">pets avaible for adoption in North America.</h1>
+                    <h1 className="jumbotron-heading2" hidden={!isLoaded}>{calgaryNumberOfAnimals}</h1>
+                    <h1 className="jumbotron-heading">of them are in Calgary.</h1>
                     <p className="lead text-muted">
                         According to Petfinder.
                     </p>
-                    <h1 className="jumbotron-heading">Check out some shelters in Calgary</h1>
+                    <h1 className="jumbotron-heading">Check out some shelters in the City</h1>
                 </Container>
             </Jumbotron>
             <Album album={album} />
